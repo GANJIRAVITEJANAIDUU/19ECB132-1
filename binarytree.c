b5e6272475327841caeedb4b3f0ca616d097c501
GANJIRAVITEJANAIDUU <102643674+GANJIRAVITEJANAIDUU@users.noreply.github.com>
2022-05-16 09:54:25 +0530

    Update binarytree.c

--- binarytree.c
+++ binarytree.c
@@ -2,201 +2,82 @@
 #include <stdlib.h>
 
 struct node {
-    int data; //node will store an integer
-    struct node *right_child; // right child
-    struct node *left_child; // left child
+  int item;
+  struct node* left;
+  struct node* right;
 };
 
-struct node* search(struct node *root, int x) {
-    if(root==NULL || root->data==x) //if root->data is x then the element is found
-        return root;
-    else if(x>root->data) // x is greater, so we will search the right subtree
-        return search(root->right_child, x);
-    else //x is smaller than the data, so we will search the left subtree
-        return search(root->left_child,x);
+// Inorder traversal
+void inorderTraversal(struct node* root) {
+  if (root == NULL) 
+    return;
+  inorderTraversal(root->left);
+  printf("%d ->", root->item);
+  inorderTraversal(root->right);
 }
 
-//function to find the minimum value in a node
-struct node* find_minimum(struct node *root) {
-    if(root == NULL)
-        return NULL;
-    else if(root->left_child != NULL) // node with minimum value will have no left child
-        return find_minimum(root->left_child); // left most element will be minimum
-    return root;
+// Preorder traversal
+void preorderTraversal(struct node* root) {
+  if (root == NULL) 
+    return;
+  printf("%d ->", root->item);
+  preorderTraversal(root->left);
+  preorderTraversal(root->right);
 }
 
-//function to create a node
-struct node* new_node(int x) {
-    struct node *p;
-    p = malloc(sizeof(struct node));
-    p->data = x;
-    p->left_child = NULL;
-    p->right_child = NULL;
-
-    return p;
+// Postorder traversal
+void postorderTraversal(struct node* root) {
+  if (root == NULL) 
+    return;
+  postorderTraversal(root->left);
+  postorderTraversal(root->right);
+  printf("%d ->", root->item);
 }
 
-struct node* insert(struct node *root, int x) {
-    //searching for the place to insert
-    if(root==NULL)
-        return new_node(x);
-    else if(x>root->data) // x is greater. Should be inserted to right
-        root->right_child = insert(root->right_child, x);
-    else // x is smaller should be inserted to left
-        root->left_child = insert(root->left_child,x);
-    return root;
-}
-// funnction to delete a node
-struct node* delete(struct node *root, int x) {
-    //searching for the item to be deleted
-    if(root==NULL)
-        return NULL;
-    if (x>root->data)
-        root->right_child = delete(root->right_child, x);
-    else if(x<root->data)
-        root->left_child = delete(root->left_child, x);
-    else
-    {
-        //No Children
-        if(root->left_child==NULL && root->right_child==NULL)
-        {
-            free(root);
-            return NULL;
-        }
+// Create a new Node
+struct node* createNode(int value) {
+  struct node* newNode = malloc(sizeof(struct node));
+  newNode->item = value;
+  newNode->left = NULL;
+  newNode->right = NULL;
 
-        //One Child
-        else if(root->left_child==NULL || root->right_child==NULL)
-        {
-            struct node *temp;
-            if(root->left_child==NULL)
-                temp = root->right_child;
-            else
-                temp = root->left_child;
-            free(root);
-            return temp;
-        }
-
-        //Two Children
-        else
-        {
-            struct node *temp = find_minimum(root->right_child);
-            root->data = temp->data;
-            root->right_child = delete(root->right_child, temp->data);
-        }
-    }
-    return root;
+  return newNode;
 }
 
-void inorder(struct node *root) {
-    if(root!=NULL) // checking if the root is not null
-    {
-        inorder(root->left_child); // visiting left child
-        printf(" %d ", root->data); // printing data at root
-        inorder(root->right_child);// visiting right child
-    }
+// Insert on the left of the node
+struct node* insertLeft(struct node* root, int value) {
+  root->left = createNode(value);
+  return root->left;
+}
+
+// Insert on the right of the node
+struct node* insertRight(struct node* root, int value) {
+  root->right = createNode(value);
+  return root->right;
 }
 
 int main() {
-    /*
-                   20
-                 /    \
-                /      \
-               5       30
-             /   \     /\
-            /     \   /  \
-           1      15 25  40
-                /          \
-               /            \
-              9             45
-            /   \          /
-           /     \        /
-          7      12      42
-    */
-    struct node *root;
-    root = new_node(20);
-    insert(root,5);
-    insert(root,1);
-    insert(root,15);
-    insert(root,9);
-    insert(root,7);
-    insert(root,12);
-    insert(root,30);
-    insert(root,25);
-    insert(root,40);
-    insert(root, 45);
-    insert(root, 42);
+  struct node* root = createNode(1);
+  insertLeft(root, 2);
+  insertRight(root, 3);
+  insertLeft(root->left, 4);
 
-    inorder(root);
-    printf("\n");
+  printf("Inorder traversal \n");
+  inorderTraversal(root);
 
-    root = delete(root, 1);
-    /*
-                   20
-                 /    \
-                /      \
-               5       30
-                 \     /\
-                  \   /  \
-                  15 25   40
-                /           \
-               /             \
-              9              45
-            /   \           /
-           /     \         /
-          7      12       42
-    */
+  printf("\nPreorder traversal \n");
+  preorderTraversal(root);
 
-    root = delete(root, 40);
-    /*
-                   20
-                 /    \
-                /      \
-               5       30
-                 \     /\
-                  \   /  \
-                  15 25  45
-                 /       / 
-                /       /   
-               9       42    
-             /   \          
-            /     \        
-           7      12      
-    */
-
-    root = delete(root, 45);
-    /*
-                   20
-                 /    \
-                /      \
-               5       30
-                 \     /\
-                  \   /  \
-                  15 25  42
-                 /          
-                /            
-               9            
-             /   \          
-            /     \        
-           7      12      
-    */
-    root = delete(root, 9);
-    inorder(root);
-    /*
-                   20
-                 /    \
-                /      \
-               5       30
-                 \     /\
-                  \   /  \
-                  15 25  42
-                 /          
-                /            
-               12            
-             /             
-            /             
-           7            
-    */
-    printf("\n");
-
-    return 0;
+  printf("\nPostorder traversal \n");
+  postorderTraversal(root);
 }
+
+
+/* Output
+Inorder traversal 
+4 ->2 ->1 ->3 ->
+Preorder traversal 
+1 ->2 ->4 ->3 ->
+Postorder traversal 
+4 ->2 ->3 ->1 ->
+*/  
